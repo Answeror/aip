f5b4ea7b84e581f58ab4cb33e7c9c0525a566857
answeror <answeror@gmail.com>
2013-07-01 00:04:49 +0800

    zip is not safe

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ from setuptools import setup
 
 setup(
     name='aip.core',
-    version='0.1.1',
+    version='0.1.2',
     author='answeror',
     author_email='answeror@gmail.com',
     packages=['aip'],
     description='AIP Instrumentality Project',
-    include_package_data=True
+    include_package_data=True,
+    zip_safe=False
 )
