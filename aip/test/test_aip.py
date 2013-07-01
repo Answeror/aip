6ca8e5aae63878a45f1ba637c06460fff29e84c8
answeror <answeror@gmail.com>
2013-07-01 19:02:08 +0800

    refector test

--- aip/test/test_aip.py
+++ aip/test/test_aip.py
@@ -9,7 +9,7 @@ from .. import make
 from .settings import CONFIG_FILE_PATH
 
 
-class TestAip(object):
+class TestBase(object):
 
     def setUp(self):
         app = Flask(__name__)
@@ -17,15 +17,25 @@ class TestAip(object):
         app.register_blueprint(self.aip)
         self.app = app.test_client()
 
-    def tearDown(self):
-        pass
+
+class TestConfig(object):
+
+    def setUp(self):
+        TestBase.setUp(self)
+
+    def test_config(self):
+        self.aip.config(CONFIG_FILE_PATH)
+        assert_equal(len(self.aip.providers), 2)
+
+
+class TestConfigured(TestBase):
+
+    def setUp(self):
+        TestBase.setUp(self)
+        self.aip.config(CONFIG_FILE_PATH)
 
     def test_index_empty(self):
         r = self.app.get('/')
         soup = Soup(r.data)
         content = soup.find(id='items').get_text().strip()
         return assert_equal(content, '')
-
-    def test_config(self):
-        self.aip.config(CONFIG_FILE_PATH)
-        assert_equal(len(self.aip.providers), 2)
