4294b4194d4cc89a238c4d0ebe507fda6b48ee73
answeror <answeror@gmail.com>
2013-07-03 01:08:22 +0800

    include scss in install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='aip.core',
-    version='0.1.4',
+    version='0.1.5',
     author='answeror',
     author_email='answeror@gmail.com',
     packages=['aip'],
