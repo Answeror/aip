b08d0126920f3e5e7b82f57f156619a08c8bc2f2
answeror <answeror@gmail.com>
2013-07-29 21:50:38 +0800

    0.1.50

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='aip.core',
-    version='0.1.49',
+    version='0.1.50',
     author='answeror',
     author_email='answeror@gmail.com',
     packages=['aip'],
