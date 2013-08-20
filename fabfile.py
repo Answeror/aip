c184564d3b93975d52b953628c89636de3ee43ea
answeror <answeror@gmail.com>
2013-08-20 23:42:19 +0800

    fab runnable, use py27 env

--- fabfile.py
+++ fabfile.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
-from fabric.api import run, env, cd
+from fabric.api import run, env, cd, prefix
 
 
 # the user to use for the remote commands
 env.user = 'answeror'
 # the servers where the commands are executed
-env.hosts = ['aip.io']
+env.hosts = ['aip.io:3843']
 
 
 def deploy():
-    run('pyenv virtualenvwrapper')
-    run('workon aip')
     with cd('/www/aip/repo'):
         run('git pull')
-        run('python setup.py develop')
+        with prefix('pyenv virtualenvwrapper'):
+            with prefix('workon aip'):
+                run('python setup.py develop')
     # and finally touch the .wsgi file so that mod_wsgi triggers
     # a reload of the application
     run('touch /www/aip/repo/application.wsgi')
