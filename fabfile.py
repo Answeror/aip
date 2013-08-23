745264353db2737f5c3ce35d6ddd0e07412bb341
answeror <answeror@gmail.com>
2013-08-23 21:14:18 +0800

    update fab file

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ from fabric.api import run, env, cd, prefix
 # the user to use for the remote commands
 env.user = 'answeror'
 # the servers where the commands are executed
-env.hosts = ['aip.io:3843']
+env.hosts = ['aip.io']
 
 
 def deploy():
