509c6c0c855e1f44241ed733b996f5fd264d9524
answeror <answeror@gmail.com>
2013-10-23 01:35:02 +0800

    use wand when possible, for animated gif resize

--- aip/imfs/utils.py
+++ aip/imfs/utils.py
@@ -3,6 +3,13 @@ from io import BytesIO
 
 
 def thumbnail(data, kind, width, height):
+    try:
+        return use_wand(data, kind, width, height)
+    except:
+        return use_pil(data, kind, width, height)
+
+
+def use_pil(data, kind, width, height):
     input_stream = BytesIO(data)
     pim = PIL.Image.open(input_stream)
     pim.thumbnail(
@@ -14,3 +21,10 @@ def thumbnail(data, kind, width, height):
         pim = pim.convert('RGB')
     pim.save(output_stream, format=kind.upper())
     return output_stream.getvalue()
+
+
+def use_wand(data, kind, width, height):
+    from wand.image import Image
+    with Image(blob=data) as img:
+        img.resize(width, height)
+        return img.make_blob(kind)
