4aee220f6da5dabdb84eb8500580e3b8e77b717c
answeror <answeror@gmail.com>
2013-10-31 19:04:37 +0800

    add gunicorn fab

--- fabfile.py
+++ fabfile.py
@@ -35,3 +35,20 @@ def deploy():
     # and finally touch the .wsgi file so that mod_wsgi triggers
     # a reload of the application
     run('touch /www/aip/repo/application.wsgi')
+
+
+def gunicorn():
+    with settings(warn_only=True):
+        kill('rqworker')
+        kill('aiplog')
+        kill('gunicorn')
+    with cd('/www/aip/repo'):
+        run('git pull')
+        with prefix('pyenv virtualenvwrapper'):
+            with prefix('workon aip'):
+                run('python setup.py develop')
+                #runbg('celery -A tasks worker')
+                runbg('aiplog')
+                for i in range(8):
+                    runbg('rqworker')
+                runbg('gunicorn -w 2 --keep-alive 5 gunicorn_app:app')
