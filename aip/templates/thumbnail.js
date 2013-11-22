f14524978013eb258bdba09e1ad70b67c2440202
answeror <answeror@gmail.com>
2013-11-22 11:15:36 +0800

    don't use hotlink by default, due to slow connection to imgur in chinese education network

--- aip/templates/thumbnail.js
+++ aip/templates/thumbnail.js
@@ -6,20 +6,28 @@
         var $d = $.Deferred();
         var $item = options.$item;
         var $img = $item.find(options.selector);
-        $.get($item.data('thumbnail-link'), $.param({
-            width: Math.round($img.width())
-        })).done(function(r) {
+        var load = function(src) {
             $.aip.load_image({
                 img: $img,
-                src: r.result,
+                src: src,
                 timeout: 1e3 * {{ config['AIP_LOADING_TIMEOUT'] }},
                 reloads: $.aip.range({{ config['AIP_RELOAD_LIMIT'] }}).map(function() {
                     return $.aip.disturb(1e3 * {{ config['AIP_RELOAD_INTERVAL'] }});
                 })
             }).done($d.resolve).fail($d.reject);
-        }).fail(function() {
-            console.log('get thumbnail link of ' + $item.data('md5') + ' failed');
-        });
+        };
+        // use actual width as param
+        var p = $.param({ width: Math.round($img.width()) });
+        // try hotlink first
+        if ($item.data('thumbnail-link')) {
+            $.get($item.data('thumbnail-link'), p).done(function(r) {
+                load(r.result);
+            }).fail(function() {
+                console.log('get thumbnail link of ' + $item.data('md5') + ' failed');
+            });
+        } else {
+            load($.param.querystring($item.data('thumbnail'), p));
+        }
         return $d;
     };
 })(jQuery);
